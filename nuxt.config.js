4d8feb544682a3545d7bd928cc4450459ce1d1e5
Lynn Huang <48780224+babycomeon0129@users.noreply.github.com>
2021-11-01 17:41:38 +0800

    修改sit 環境變數

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ const envObj = {
       mobii: 'https://www-uuat.mobii.ai',
       domain: 'https://sit-events.mobii.ai',
       GA: 'GTM-KBZ2VT8',
-      host: '210.65.10.85',
+      host: '172.31.15.223',
       port: 8080,
       key: '/etc/pki/eyesmedia/server_mobii_202101.key',
       cert: '/etc/pki/eyesmedia/mobii_20210112.pem'

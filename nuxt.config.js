b21fc99c59041b0030ce24f2f1b56d2d995f67b4
Lynn Huang <48780224+babycomeon0129@users.noreply.github.com>
2021-11-01 16:26:15 +0800

    修改config

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ const envObj = {
       mobii: 'https://mobii.ai',
       domain: 'https://event.mobii.ai',
       GA: 'GTM-KBZ2VT8',
-      host: '172.31.15.221',
+      host: '210.65.10.85',
       port: 8080,
       key: '/etc/pki/eyesmedia/server_mobii_202101.key',
       cert: '/etc/pki/eyesmedia/mobii_20210112.pem'

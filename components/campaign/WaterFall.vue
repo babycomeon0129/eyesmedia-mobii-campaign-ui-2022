9160ec8981930388d5b3f71c95018bab6e261216
Lynn Huang <48780224+babycomeon0129@users.noreply.github.com>
2021-11-29 11:42:56 +0800

    MOB-3842：[一頁式活動] 增加「設定瀑布流」模組(FE)（作業中）

---
+++ components/campaign/WaterFall.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="waterfall">
+    <div class="row product">
+      <div class="col-3 col-m6">
+        123
+      </div>
+      <div class="col-3 col-m6">
+        123
+      </div>
+      <div class="col-3 col-m6">
+        123
+      </div>
+      <div class="col-3 col-m6">
+        123
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'WaterFall',
+  data () {
+    // ...
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.product {
+  .col-3 {
+    border: 1px solid red;
+  }
+}
+
+</style>

fb397fcb91115591067f9f98ffbdc5d39a7d867f
Lynn Huang <48780224+babycomeon0129@users.noreply.github.com>
2021-12-21 15:36:23 +0800

    修改成移除-而非/

--- pages/campaign/VacCard.vue
+++ pages/campaign/VacCard.vue
@@ -544,7 +544,7 @@ export default {
       this.requestData.birth_dt = `${this.birth_dt.slice(0, 2)}-${this.birth_dt.slice(2)}`;
       const date = this.$moment(`2021-${this.requestData.birth_dt}`);
       this.verify.birth_dt = date.isValid();
-      this.requestData.birth_dt = this.verify.birth_dt ? this.birth_dt.replace('/', '') : null;
+      this.requestData.birth_dt = this.verify.birth_dt ? this.birth_dt.replace('-', '') : null;
       // 檢查verify內的東西是否都是true
       const submitOk = Object.values(this.verify).every(e => e === true);
       // TODO:上SIT記得補上機器人驗證

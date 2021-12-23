45e8cb485de2ff66b4169c367158f484458af732
Lynn Huang <48780224+babycomeon0129@users.noreply.github.com>
2021-12-23 10:41:42 +0800

    MOB-4003:修復身分證尾數為0時驗證不會過的問題

--- pages/campaign/VacCard.vue
+++ pages/campaign/VacCard.vue
@@ -659,7 +659,8 @@ export default {
         checkSum += c * w;
       }
       // 算出的檢查碼比對身分證最後一個檢查碼
-      return 10 - checkSum % 10 === checkPid;
+      // (10 - checkSum % 10) % 10是為了防尾數0，10-0的情況還是10
+      return (10 - checkSum % 10) % 10 === checkPid;
     }
   }
 };
